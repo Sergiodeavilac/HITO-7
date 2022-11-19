282ca4c140dd7b057f5c368b52d27c888eaeadac
Sergiodeavilac <sergiodeavilacabral@gmail.com>
2022-11-19 13:23:20 +0100

    Update

---
+++ Sources/Forces_models.py
@@ -0,0 +1,40 @@
+from load_gmat import *
+
+# -----------------------------------------------------------
+# Forces Model 
+# -----------------------------------------------------------
+def Forces():
+    fm = gmat.Construct("ForceModel", "TheForces")
+
+    # An 8x8 JGM-3 Gravity Model
+    earthgrav = gmat.Construct("GravityField")
+    earthgrav.SetField("BodyName","Earth")
+    earthgrav.SetField("Degree",8)
+    earthgrav.SetField("Order",8)
+    earthgrav.SetField("PotentialFile","JGM2.cof")
+
+    # The Point Masses
+    moongrav = gmat.Construct("PointMassForce")
+    moongrav.SetField("BodyName","Luna")
+    sungrav = gmat.Construct("PointMassForce")
+    sungrav.SetField("BodyName","Sun")
+
+    # Drag using Jacchia-Roberts
+    jrdrag = gmat.Construct("DragForce")
+    jrdrag.SetField("AtmosphereModel","JacchiaRoberts")
+
+    # Build and set the atmosphere for the model
+    atmos = gmat.Construct("JacchiaRoberts")
+    jrdrag.SetReference(atmos)
+
+    # Add all of the forces into the ODEModel container
+    fm.AddForce(moongrav)
+    fm.AddForce(sungrav)
+    fm.AddForce(jrdrag)
+    fm.AddForce(earthgrav)
+    
+    print(fm.Help())
+    return(fm)
+
+if __name__ == '__main__': 
+    Forces
---
+++ Sources/Main.py
@@ -0,0 +1,45 @@
+from Forces_models import Forces
+from load_gmat import *
+
+
+# -----------------------------------------------------------
+# Forces Model 
+# -----------------------------------------------------------
+
+fm = Forces
+
+
+
+
+# -----------------------------------------------------------
+# Our spacecraft 
+# -----------------------------------------------------------
+
+mysat = gmat.Construct("Spacecraft","Leosat")
+mysat.SetField("DateFormat", "UTCGregorian")
+mysat.SetField("Epoch", "12 Mar 2020 15:00:00.000")
+mysat.SetField("CoordinateSystem", "EarthMJ2000Eq")
+mysat.SetField("DisplayStateType", "Keplerian")
+mysat.SetField("SMA", 7005)
+mysat.SetField("ECC", 0.008)
+mysat.SetField("INC", 28.5)
+mysat.SetField("RAAN", 75)
+mysat.SetField("AOP", 90)
+mysat.SetField("TA", 45)
+
+mysat.SetField("DryMass", 50)
+mysat.SetField("Cd", 2.2)
+mysat.SetField("Cr", 1.8)
+mysat.SetField("DragArea", 1.5)
+mysat.SetField("SRPArea", 1.2)
+
+
+# -----------------------------------------------------------
+# Import data DiscosWeb
+# -----------------------------------------------------------
+
+
+# Propagate
+
+
+
