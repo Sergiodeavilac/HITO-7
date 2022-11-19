5e64567ee779c77c2f901a0f37dedaa068cd247c
Andres <andres.gserrano@alumnos.upm.es>
2022-11-19 19:24:07 +0100

    fix: Improve and fix minor errors

--- tests/testFilters.py
+++ tests/testFilters.py
@@ -5,6 +5,7 @@
 import unittest
 from Sources.DISCOSWeb.filters import ObjectClassFilter, ObjectClassType,\
     OrbitAttribute, OrbitFilter, FilterOperation
+from Sources.DISCOSWeb.filtering import APIFilter
 
 
 
@@ -61,6 +62,32 @@ class TestOrbit(unittest.TestCase):
 
         # Test epoch.
         self.assertEqual( OrbitFilter(OrbitAttribute.Epoch, FilterOperation.Equal, "2020-31-01").apistring(), "eq(destinationOrbits.epoch,2020-31-01)")
+
+
+
+class TestAPIFilter(unittest.TestCase):
+    def test_FullFilter(self):
+        """Tests a full filter API URL"""
+
+        # Build the filter.
+        filter = APIFilter()
+
+        # Add object classes.
+        filter.addObjectClass( ObjectClassType.Payload )
+        filter.addObjectClass( ObjectClassType.Debris )
+
+        # Build the Orbit requirements.
+        sma = OrbitFilter( OrbitAttribute.SemimajorAxis, FilterOperation.GreaterEqual, 9000 )
+        incs = OrbitFilter( OrbitAttribute.Inclination, FilterOperation.GreaterEqual, 45.0 )
+        incm = OrbitFilter( OrbitAttribute.Inclination, FilterOperation.LessEqual, 65.0 )
+
+        # Add orbit requirements.
+        filter.addOrbitFilter(sma)
+        filter.addOrbitFilter(incs)
+        filter.addOrbitFilter(incm)
+
+        self.assertEqual( filter.apistring(), "ne(destinationOrbits,null)&in(objectClass,('Other Debris','Payload'))&ge(destinationOrbits.sma,9000)&ge(destinationOrbits.inc,45.00)&le(destinationOrbits.inc,65.00)" )
+
 
 if __name__ == "__main__":
     unittest.main()
